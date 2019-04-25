0af1fc95503a4afc30b22905683cb7804d19aaa8
Claus Niesen <cniesen@users.noreply.github.com>
2019-04-25 12:53:51 -0500

    Updated to okta-signin-widget 2.19.0 and brought dependencies up to date

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ Development (local)
     npm install
     ```
 
+3. Update the `config.json` configuration file.
+
 4. Start webpack development server
 
     ```
@@ -24,26 +26,20 @@ Development (local)
 
 4. Point browser to http://localhost:3000
 
-    ```
-    npm start
-    ```
-
-Preview Deployment
-------------------
-Use the Jenkins job.  The `npm run build-preview` command build the login widget for the preview 
-environment.  The config-preview.json configurations are used.  Deploy all the files from the `dist/`
-directory to the server.
-
-Preview Deployment
-------------------
-Use the Jenkins job.  The `npm run build-production` command build the login widget for the production 
-environment.  The config-production.json configurations are used.  Deploy all the files from the `dist/`
-directory to the server.
 
 Gradle
 ------
 The npm tasks can be started via gradle.  The benefit is that node/npm doesn't need to be installed.  The cost is a longer build time.
 
 - `gradle start` Deploys local development server and listens on http://localhost:3000.
-- `gradle buildPreview` Builds the preview deployment.
-- `gradle buildProduction` Builds the production deployment.
+- `gradle buildDist` Builds the production deployment.
+
+Known Issues
+------------
+
+* The okta-signin-widget uses an outdated jQuery version with the CVE-2015-9251 security alert.  Okta is aware of this 
+  but has not fixed the issue so far. https://github.com/okta/okta-signin-widget/issues/378 (The default Okta login page
+  also the old version of jQuery.)
+
+* bundle.js is large with 1.3 MB.  The size is due to the Okta dependency (their code is huge).  Okta's CDN file isn't any
+  smaller.  Even the default Okta login page includes a 1.5 MB js file.
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-var path = require('path');
+const path = require('path');
 var webpack = require('webpack');
-var autoprefixer = require('autoprefixer')
 
 module.exports = {
     entry: [
@@ -11,30 +10,23 @@ module.exports = {
         publicPath: '/',
         filename: 'bundle.js'
     },
-    node: {
-        fs: 'empty'
-    },
     module: {
         rules: [
             {
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: [ 'es2015', 'stage-1'],
-                    plugins: ['babel-plugin-add-module-exports']
-                }
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             },
-            {test: /\.json$/, use: [{loader: 'json-loader'}]},
-            {test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: [{loader: 'file-loader'}]},
-            {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: [{loader: 'url-loader?limit=10000&mimetype=application/font-woff'}]},
-            {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: [{loader: 'url-loader?limit=10000&mimetype=application/octet-stream'}]},
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}]},
-            {test: /\.(jpe?g|png|gif)$/i, use: [{loader: 'file-loader?name=[name].[ext]'}]},
-            {test: /(\.css|\.scss|\.sass)$/, use: [{loader: 'style-loader'}, {loader: 'css-loader'}, {loader: 'postcss-loader', options: {plugins: function() {return [autoprefixer]}}}, {loader: 'sass-loader'}]}
-        ]
+            {
+                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                },
+            },
+        ],
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js']
     },
     devServer: {
         historyApiFallback: true,
