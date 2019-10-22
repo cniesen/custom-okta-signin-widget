b79706e89d73a9768e92599b221b8cc4b20151e8
Claus Niesen <cniesen@users.noreply.github.com>
2019-10-22 10:41:17 -0500

    Updated to okta-signin-widget 3.3.3 and brought dependencies up to date'

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import getRedirectUrl from './getRedirectUrl';
 import backgroundWatermark from './backgroundWatermark';
 
 // ### The OktaSignIn module doesn't work properly so we use CDN.
+// ### (This statement is outdated. A reevaluation is suggested.  However, since CDN works pretty nicely no recent attempts have been made)
 // import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 // import '@okta/okta-signin-widget/dist/css/okta-theme.css';
 // import OktaSignIn from '@okta/okta-signin-widget';
@@ -14,19 +15,21 @@ const redirect_url = getRedirectUrl();
 backgroundWatermark(configFile.watermarkText);
 
 var oktaSignIn = new OktaSignIn(configFile.OktaSignInWidget);
-oktaSignIn.session.exists(function (exists) {
-
-    // Session found...
-    if (exists) {
-        window.location = redirect_url;
-    } else {
-        oktaSignIn.renderEl(
-            {el: '#okta-login-container'},
-            function success(res) {
-                if (res.status === 'SUCCESS') {
-                    res.session.setCookieAndRedirect(redirect_url);
+oktaSignIn.authClient.session.exists()
+    .then(function(exists) {
+        if (exists) {
+            // logged in
+            window.location = redirect_url;
+        } else {
+            // not logged in
+            oktaSignIn.renderEl(
+                {el: '#okta-login-container'},
+                function success(res) {
+                    if (res.status === 'SUCCESS') {
+                        res.session.setCookieAndRedirect(redirect_url);
+                    }
                 }
-            }
-        );
-    }
-});
+            );
+        }
+
+    });
